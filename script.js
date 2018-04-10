50ad77bf54ac362755eba72654eca787ec0221ce
kyshongrandy <kyshon53@gmail.com>
2018-04-10 22:23:49 +0000

    changes

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $.ajax({
+  url : "https://api.wunderground.com/api/db98605a355b736f/geolookup/conditions/q/IA/Cedar_Rapids.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_f = parsed_json['current_observation']['temp_f'];
+  alert("Current temperature in " + location + " is: " + temp_f);
+ 
+  }
+  });
+});
+<input id="text">
+<button> go! </button>
+$("#button").click(function(){
+    var message = $("#search").val(); 
+});
+$("div").append("");
+
+function GetRandomWeatherChoice() {
+  var weather = ["Rain",  "snow", "Windy", "Sunny", "stormy"];
+}
+
+ backgroundChange
+function updatebackground (){
+        // document.getElementById("airPlane").src 
+            if(CURRENTENVIRONMENT.appearance=== "rain"){
+                backgroundChange("rain1.jpg")
+            } else if(CURRENTENVIRONMENT.appearance=== "Sunny"){
+                backgroundChange("sunny1.jpg")
+            } else if(CURRENTENVIRONMENT.appearance=== "Snow"){
+                backgroundChange("snow1.jpg")
+            }  else if(CURRENTENVIRONMENT.appearance=== "Windy"){
+            backgroundChange("windy1.jpg")
+            } else if(CURRENTENVIRONMENT.appearance=== "stormy"){
+              backgroundChange("stormy.jpg")
+            }
+}
